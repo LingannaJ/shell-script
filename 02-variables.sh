906ddbee9b9b7410a4450380f55d5db264ef0bba
LingannaJ <jlinganna104@gmail.com>
2024-04-19 12:16:42 +0530

    frist shell-script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,7 @@
-this variables help to find locatins
+#!/bin/bash
+
+echo "chikki:: hello chitti how are you?"
+echo "chitti:: Hi chikki I am fine How are you?"
+echo "chikki:: Am good how is your work?"
+echo "Chitti:: Its Good am learning devops"
+echo "Chikki:: where you are learning"
