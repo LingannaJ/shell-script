5b598ce334f31593022ad0121d1c71571876aadc
LingannaJ <jlinganna104@gmail.com>
2024-04-20 10:47:23 +0530

    my-shell-practice

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -2,7 +2,7 @@
 
 echo "All variables:$@"
 echo "Number of variables passed: $#"
-echo "Script Name: $0
+echo "Script Name: $0"
 echo "Current working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "Which user is running this script: $USER"
