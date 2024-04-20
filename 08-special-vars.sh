890c626303d2604292de96c85a972d00c35b7bd0
LingannaJ <jlinganna104@gmail.com>
2024-04-20 10:45:42 +0530

    my-shell-practice

---
+++ 08-special-vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "All variables:$@"
+echo "Number of variables passed: $#"
+echo "Script Name: $0
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Host Name: $HOSTNAME"
+echo "Prosess ID of the current shell script: $$"
