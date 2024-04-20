3cd00724ca1d3462eb03ffac3c03e85c28b31382
LingannaJ <jlinganna104@gmail.com>
2024-04-20 09:22:24 +0530

    my-shell-practice

---
+++ 06-array.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+GODS=("Shiva" "Rama" "Krishna")
+
+#ikkada manam above array list anedhi stars from 0 , nundi start avthundi ante shiva=0 rama=1 krishna=2 ila print avuthundhi
+
+#kani above array size matram 3 avthundhi , size 1 nundi consider chesthundhi
+
+#index anedhi kuda 0 nundi start avthundhi 0,1,2, ila 
+
+echo "frist god is ${Gods[0]}"
+echo "frist god is ${Gods[1]}"
+echo "frist god is ${Gods[2]}"
+echo "frist god is ${Gods[@]}"
