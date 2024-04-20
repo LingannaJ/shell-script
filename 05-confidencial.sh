b08336ec0918a8a5b7cd3373dcdeb563be7c79c5
LingannaJ <jlinganna104@gmail.com>
2024-04-20 08:47:10 +0530

    my-shell-practice

---
+++ 05-confidencial.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please Enter username::"
+
+read USERNAME #ikkada username anedhi variable 
+
+echo "Please Enter password::"
+
+read PASSWORD
+
+echo "Username is: $USERNAME, Password is: $PASSWORD
