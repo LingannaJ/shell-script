aaa28dcca9d9ffee8e0258d37e32b9caee489215
LingannaJ <jlinganna104@gmail.com>
2024-04-20 12:08:13 +0530

    my-shell-practice

--- 10-installation.sh
+++ 10-installation.sh
@@ -10,16 +10,25 @@ else
     echo "you are super user."
 fi
 
-dnf install mysqll -y
+dnf install mysql -y
 
 if [ $? -ne 0 ]
 then 
 echo "Installation of mysql.... FAILURE"
 exit 1
+else 
+  echo "Installation of mysql.... Success"
 fi
 
 dnf install git -y
 
+
+if [ $? -ne 0 ]
+then 
+echo "Installation of git.... FAILURE"
+exit 1
+else
+   echo "Installation of git..... Success"
+fi
+
 echo "is script proceeding?"
-
-
