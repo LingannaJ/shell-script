e9661311d61e869e569e42e3ed4ff3b71a3dd1b5
LingannaJ <jlinganna104@gmail.com>
2024-04-20 08:12:19 +0530

    my-shell-practice

--- 01-hello-world.sh
+++
@@ -1,2 +0,0 @@
-hi am learning devops
-practicing sessions are running
---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+PERSON1=Chikki #space anedhi undakudashu = ki value ki madyalo
+PERSON2=Chitti 
+
+
+echo "$PERSON1:: hello $PERSON2, how are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine How are you?"
+echo "$PERSON1:: Am good how is your work?"
+echo "$PERSON2:: Its Good am learning devops"
+echo "$PERSON1:: where you are learning"
