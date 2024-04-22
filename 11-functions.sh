387798b97e84a0bb072ccc589e8ccac357f8895e
LingannaJ <jlinganna104@gmail.com>
2024-04-22 09:20:20 +0530

    my shell-practice

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 TIMESTUMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$( $0 | cut -d "." f1)
+SCRIPT_NAME=$( echo $0 | cut -d "." f1)
 LOGFILE=/tem/$SCRIPT_NAME-TIMESTUMP.log
 
 VALIDATE (){
