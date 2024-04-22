fb634e23bfe49da2aac935f287ea0ce625e0d025
LingannaJ <jlinganna104@gmail.com>
2024-04-22 09:37:34 +0530

    my shell-practice

--- 11-functions.sh
+++ 11-functions.sh
@@ -3,12 +3,12 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
-LOGFILE=/tem/$SCRIPT_NAME-TIMESTAMP.log
+LOGFILE=/tmp/$SCRIPT_NAME-TIMESTAMP.log
 
 VALIDATE (){
     if [ $1 -ne 0 ]
     then echo "$2... FAILURE"
-       Exit 1
+       exit 1
     else
        echo "$2.... SUCCESS"
     fi
